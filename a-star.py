638a7cf8e0955e8b0b9bc6308e3fef51822392c2
Tommy Long <61881891+Tommy-Long4358@users.noreply.github.com>
2022-09-05 01:30:22 -0700

    Update a-star.py
    backtracking solved

--- a-star.py
+++ a-star.py
@@ -1,29 +1,28 @@
 import math
-
 '''
-Graph Traversal Algorithm that goes from the initialCity to the arrivingCity in the least optimal amount of distance in miles.
+Graph Traversal Algorithm that goes from the initialCity to the arrivingCity in the optimal amount of distance in miles.
 It's a combination of Dijkstra's Algorithm + Greedy Search Algorithm.
 '''
 def a_star(initialCity, arrivingCity):
-    # Path from initialCity to arrivingCity
+    # Optimal path from initialCity to arrivingCity
     traversal_path = [initialCity]
     
-    # Cities that have been traversed
+    # Cities that have been visited
     seenCities = [initialCity]
 
-    # List of cities to be compared to for least distance travelled and that haven't been traversed through 
+    # List of cities to be compared to for least distance travelled
     distanceQueue = []
 
-    # List of all paths taken with backtracking
+    # List of all paths taken
     allPaths = []
 
     # Minimum distance it takes to get to a city
     minimumDistance = None
     minimumCity = ""
 
-    # Loop until initialCity is equal to arrivedCity and end loop
+    # Loop until initialCity is equal to arrivedCity and end loop there
     while initialCity != arrivingCity:
-        # nested loop through all nearest cities of currentCity to find SLD (straight line distance) 
+        # Loop through all nearest cities of a given city from the dictionary
         for city in mapDict[initialCity]:
             # Add city to list of seen cities if not visited, skip the city if it has been visited
             if city[0] in seenCities:
@@ -31,30 +30,34 @@ def a_star(initialCity, arrivingCity):
             else:
                 seenCities.append(city[0])
 
-            # Check SLD amount from initialcity to all city neighbors
+            # Straight Line Distance amount from current city to destination
             SLD = haversineFormulaConversion(coordinateDict[city[0]], coordinateDict[arrivingCity])
-            
-            # Add distance with SLD
-            distance = SLD + float(city[1])
 
-            # Add distance to list of cities visited
+            # Add distance with SLD
+            # f(n) = g(n) + h(n)
+            distance = "{0:.2f}".format((city[1] + distance_calculator(traversal_path)) + SLD)
+
+            #print(city[0], ":", city[1] + distance_calculator(traversal_path), " + ", SLD, " = ", distance)
+
+            # Add distance to queue
             distanceQueue.append([distance, city[0]])
 
             # Find minimum distance out of all neighbors of the current city
-            if minimumDistance is None or distance < minimumDistance:
+            if not minimumDistance or distance < minimumDistance:
                 minimumDistance = distance
                 minimumCity = city[0]
 
         # Find the city with the minimum distance in queue currently
         element = min(distanceQueue)
 
-        # If the element is less than the neighboring minimum distance found, that means the element is found elsewhere in the graph and is not a neighbor of the current city.
+        # If the element distance is less than the neighboring minimum distance found, 
+        # that means the element is found elsewhere in the graph and is not a neighbor of that current city.
         if not minimumDistance or element[0] < minimumDistance:
             minimumCity = element[1]
 
             # Add path to list of paths traversed
             allPaths.append(traversal_path)
-
+            
             # Find a path to that minimum city
             traversal_path = correction_path(allPaths, minimumCity)
 
@@ -68,16 +71,21 @@ def a_star(initialCity, arrivingCity):
         initialCity = minimumCity  
         minimumDistance = None
 
+        #print(traversal_path)
+        
         traversal_path.append(initialCity)
-
+        
+        #print(traversal_path)
+        #print()
+        
     return traversal_path
 
 '''
 This function works by how it checks a given path in allPaths, checks to see if a given 
-city in the path has the startingPoint as its neighbor and appends the city until startingPoint is found.
-Returns the path taken to that startingPoint.
+city in the path has the targetCity as its neighbor and appends that city until targetCity is found.
+Returns the path taken to that targetCity.
 '''
-def correction_path(allPaths, startingPoint):
+def correction_path(allPaths, targetCity):
     newPath = []
     
     for path in allPaths:
@@ -85,17 +93,18 @@ def correction_path(allPaths, startingPoint):
             newPath.append(city)
 
             for neighbor in mapDict[city]:
-                if neighbor[0] == startingPoint:
+                if neighbor[0] == targetCity:
                     return newPath
 
-        # Clear the list only if the path taken doesn't have the startingPoint as a neighbor in any of the cities in the path.
+        # Clear the list only if the path taken doesn't have the 
+        # targetCity as a neighbor in any of the cities in the path.
         newPath.clear()
             
     return newPath
 
 '''
 Distance calculator for the path taken 
-from starting point to the destination.
+from starting point to the destination in the list.
 '''
 def distance_calculator(traversal_path):
     distance = 0
@@ -107,7 +116,7 @@ def distance_calculator(traversal_path):
             if traversal_path[i + 1] == city[0]:
                 distance += city[1]
     
-    return distance
+    return float("{0:.2f}".format(distance))
 
 '''
 Convert latitude and longitude to be the straight line distance between
@@ -121,8 +130,8 @@ def haversineFormulaConversion(city1Coordinate, city2Coordinate):
     r = 3958.8
 
     distance = 2 * r * math.asin(math.sqrt( (math.sin((p2 - p1) / 2) ** 2) + (math.cos(p1) * math.cos(p2)) * (math.sin((a2 - a1) / 2) ** 2)))
-    return distance
-
+    return float("{0:.2f}".format(distance))
+    
 # Reading from coordinates.txt and putting the city name in 
 # the dictionary as a key with the coordinate as a value
 coordinateFile = open('coordinates.txt', 'r')
@@ -169,31 +178,16 @@ for line in mapFile:
     
     mapDict[cityName] = arr
 
-
-# Test Case Code
-pairs = [["SanFrancisco", "LongBeach"], ["Eureka", "Monterey"], ["Monterey", "SanDiego"], ["Yosemite", "LongBeach"], ["LongBeach", "SanFrancisco"], ["Eureka", "SouthLakeTahoe"]]
-
-for pair in pairs:
-    
-    print("From city:", pair[0])
-    print("To city:", pair[1])
-
-    optimal_route = a_star(pair[0], pair[1])
-    route = ""
-    for i in range(len(optimal_route)):
-        if i == len(optimal_route) - 1:
-            route += optimal_route[i]
-            break
-
-        route += optimal_route[i] + " - "
-
-    print("Best Route:", route)
-    print("Total distance:", "{0:.2f}".format(distance_calculator(optimal_route)), "mi")
-
-    print()
-
 '''
-Actual output for assignment:
+for x, y in mapDict.items():
+    print(x, ":", y)
+'''
+
+# Actual output for assignment:
+startingCity = "LongBeach"
+arrivingCity = "SanFrancisco"
+
+optimal_route = a_star(startingCity, arrivingCity)
 
 print("From city:", startingCity)
 print("To city:", arrivingCity)
@@ -208,39 +202,3 @@ for i in range(len(optimal_route)):
 
 print("Best Route:", route)
 print("Total distance:", distance_calculator(optimal_route), "mi")
-'''
-
-'''
-Example Output:
-
-python a-star.py SanFrancisco LongBeach
-From city: SanFrancisco
-To city: LongBeach
-Best Route: SanFrancisco - SanJose - Fresno - LosAngeles - LongBeach
-Total distance: 442.50 mi
-
-From city: Eureka
-To city: Monterey
-Best Route: Eureka - SanFrancisco - SanJose - Monterey
-Total distance: 391.10 mi
-
-From city: Monterey
-To city: SanDiego
-Best Route: Monterey - SantaBarbara - SantaMonica - SanDiego
-Total distance: 416.70 mi
-
-From city: Yosemite
-To city: LongBeach
-Best Route: Yosemite - Fresno - LosAngeles - LongBeach
-Total distance: 306.80 mi
-
-From city: LongBeach
-To city: SanFrancisco
-Best Route: LongBeach - LosAngeles - Fresno - SanJose - SanFrancisco
-Total distance: 442.50 mi
-
-From city: Eureka
-To city: SouthLakeTahoe
-Best Route: Eureka - Sacramento - SouthLakeTahoe
-Total distance: 392.00 mi
-'''
